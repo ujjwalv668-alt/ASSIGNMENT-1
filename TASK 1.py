89beead5100fee98564f55bd79b6b6e1cf26a28d
ujjwalv668-alt <ujjwalv668@gmail.com>
2025-12-14 14:57:53 +0530

    Add files via upload

---
+++ TASK 1.py
@@ -0,0 +1,7 @@
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+
+print("addition :", (num1 + num2))
+print("subtraction:", (num1 - num2))
+print("multiplication:", (num1 * num2))
+print("division:", (num1 / num2))
---
+++ TASK 2.py
@@ -0,0 +1,5 @@
+First_name = input("Please Enter Your First Name :")
+Last_name = input('Please Enter Your Last Name :')
+
+Greetings = ( First_name + ' ' + Last_name)
+print("Hello!", Greetings + "! welcome To The Python Program." )
